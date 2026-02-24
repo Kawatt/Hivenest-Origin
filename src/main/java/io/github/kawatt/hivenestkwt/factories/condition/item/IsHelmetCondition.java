d84e7e5dea9c156bb3764c21801ad99b4a254067
Kawatt <99446786+Kawatt@users.noreply.github.com>
2026-02-24 23:02:50 +0100

    removing unused imports

--- src/main/java/io/github/kawatt/hivenestkwt/factories/condition/item/IsHelmetCondition.java
+++ src/main/java/io/github/kawatt/hivenestkwt/factories/condition/item/IsHelmetCondition.java
@@ -1,15 +1,8 @@
 package io.github.kawatt.hivenestkwt.factories.condition.item;
 
-import io.github.apace100.apoli.component.PowerHolderComponent;
-import io.github.apace100.apoli.data.ApoliDataTypes;
-import io.github.apace100.apoli.power.PowerType;
 import io.github.apace100.apoli.power.factory.condition.ConditionFactory;
-import io.github.apace100.apoli.util.Comparison;
 import io.github.apace100.calio.data.SerializableData;
-import io.github.apace100.calio.data.SerializableDataTypes;
 import io.github.kawatt.hivenestkwt.Hivenest;
-import io.github.kawatt.hivenestkwt.factories.power.EntityStoragePower;
-import net.minecraft.entity.Entity;
 import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.item.ArmorItem;
 import net.minecraft.item.Item;
